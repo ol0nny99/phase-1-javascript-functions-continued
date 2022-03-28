bb998298097f6b310febbf082ab483a7ea41b10b
ol0nny99 <100658149+ol0nny99@users.noreply.github.com>
2022-03-28 12:26:22 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+// Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+
+
+const mondayWork = function(activity= "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+
+
+function wrapAdjective(sym) {
+    if((sym == "*") || (sym =="||")) {
+        return (adjective = "a dedicated programmer") => {
+            return `You are ${sym}${adjective}${sym}!`
+        }
+    }
+    
+}
